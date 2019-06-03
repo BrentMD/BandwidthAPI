025baee76a460b6cee206288edbbc44c83d12ee5
Brent Day <brentdphone@gmail.com>
2019-06-03 11:43:49 -0500

    Removed initial run and started with the While loop to clean up duplicate code

--- VIPbandwidth.py
+++ VIPbandwidth.py
@@ -4,29 +4,22 @@ import requests
 # Bandwidth Account Variables
 userName = raw_input("Bandwidth Username: ")
 userPassword = raw_input("Bandwidth Password: ")
-subAccountID = raw_input("Sub Account ID: ")
-locationID = raw_input("Location ID: ")
 
 # api-endpoint 
-URL = ("https://dashboard.bandwidth.com/api/accounts/5004683/sites/" + subAccountID + "/sippeers/" + locationID)
+URL = ("https://dashboard.bandwidth.com/api/accounts/5004683/sites/")
 payload = ""
 headers = {
     'Content-Type': "application/xml",
     'Connection': "keep-alive",
     'cache-control': "no-cache"
     }
-
-# Get Request using 'requests'
-response = requests.get(URL, auth=(userName, userPassword), data=payload, headers=headers)
-
-# Print out of Get request
-print(response.text)
-
-# New Lookup
-newLookup = raw_input("Would you like to look up another(y,n)? ")
+# Begin Lookup
+newLookup = 'y'
 while newLookup == 'y':
     subAccountID = raw_input("Sub Account ID: ")
     locationID = raw_input("Location ID: ")
-    response = requests.get(URL, auth=(userName, userPassword), data=payload, headers=headers)
+    response = requests.get(URL + subAccountID + "/sippeers/" + locationID, auth=(userName, userPassword), data=payload, headers=headers)
     print(response.text)
     newLookup = raw_input("Would you like to look up another(y,n)? ")
+
+
